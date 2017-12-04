c2e3be3c1172c73dbfa7ab47d37aaaeed9287f5f
УринбасаровД <@A-305-10>
2017-12-04 14:15:32 +0600

    first

---
+++ PracktTask05/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracktTask05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Init();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Ошибка: " + e.Message);
+            }
+
+            
+
+            Console.ReadLine();
+        }
+        static void Init()
+        {
+            Console.WriteLine("Введите строку не более 6 символов: ");
+            string message = Console.ReadLine();
+            if (message.Length > 6)
+            {
+                throw new Exception("Длина строки больше 6 символов");
+            }
+
+
+        }
+
+       
+    }
+}
